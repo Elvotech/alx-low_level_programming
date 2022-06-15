337541fe69aae6dcacc8d07871c425eedd81df0f
Elvotech <kipyegonlvis@gmail.com>
2022-06-14 21:38:54 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print _putchar followed by new line
- * Return: Always 0 success
-*/
-int main (void)
+ * main - starting point
+ *
+ * Return: return 0 if successful
+ */
+int main(void)
 {
-   _putchar('_');
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
-
-   return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
